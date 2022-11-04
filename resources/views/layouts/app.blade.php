95e9e0c29d2fd32239d6623941bc4fb45f933c82
Lin-Thet-Zaw <ammoaalinthetzaw@gmail.com>
2022-11-04 17:17:03 +0630

    Fix his request has been blocked; the content must be served over HTTPS Error

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
     <!-- Fontawesome -->
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="{{ secure_url('darkly-bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ secure_url('css/darkly-bootstrap.css') }}">
     
 
 </head>
